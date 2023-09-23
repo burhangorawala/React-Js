b2d103fd41fbfce650de40da528c8cb7d632fef1
burhangorawala <gorawalab@gmail.com>
2023-09-23 15:39:02 +0530

    .

--- burhan_todo_list/app/page.js
+++ burhan_todo_list/app/page.js
@@ -13,15 +13,44 @@ const page = () => {
     setdesc("");
     console.log(mainTask);
   };
+
+  const deleteHandler = (i)=>{
+      let copyTask = [...mainTask]
+      copyTask.splice(i,1)
+      setmainTask(copyTask)
+  }
+  const toggleCompleted = (i) => {
+    // Find the task in your state or context
+    const updatedTasks = mainTask.map((task) =>
+      i === i ? { ...mainTask, completed: !mainTask.completed } : mainTask
+    );
+  
+    // Update the state with the updatedTasks
+    setTasks(updatedTasks);
+  };
+  
+ 
+  
+
   let renderTask = <h2>No Task available</h2>;
   if (mainTask.length > 0) {
     renderTask = mainTask.map((t, i) => {
       return (
-        <li>
-          <div className="flex justify-between mb-5">
+        <li key={i} className="flex items-center justify-between mb-5">
+         <input
+        type="checkbox"
+        checked={mainTask.completed}
+        onChange={() => toggleCompleted(i)}
+      />
+          <div className="flex justify-between w-2/3">
             <h5 className="text-2xl font-semibold"> {t.title} </h5>
-            <h6 className="text-xl font-semibold"> {t.desc} </h6>
+            <h6 className="text-lg font-medium"> {t.desc} </h6>
           </div>
+          <button 
+          onClick={()=>{
+            deleteHandler(i)
+          }}
+          className=" bg-red-400 text-white px-4 py-2 rounded font-bold">Delete</button>
         </li>
       );
     });
